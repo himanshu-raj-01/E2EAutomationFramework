af0a9d97bf2597123f506a31ed2e5e7eb7347ac4
himanshu raj <himanshuraj0110@gmail.com>
2026-03-04 17:13:34 +0530

    made changes

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,16 +1,18 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Optional;
 
 import factory.DriverFactory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BaseTest {
@@ -22,20 +24,28 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters("browser")
-    public void setup(String browser) {
+    public void setup(@Optional("chrome") String browser) {
 
         logger.info("Initializing browser: " + browser);
 
+        // Initialize driver
         driver = DriverFactory.initDriver(browser);
+
+        // Explicit wait
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        // Navigate to application
         driver.get("https://www.saucedemo.com/");
         logger.info("Navigated to application URL");
     }
 
     @AfterMethod
     public void tearDown() {
+
         logger.info("Closing browser");
-        DriverFactory.quitDriver();
+
+        if (driver != null) {
+            DriverFactory.quitDriver();
+        }
     }
 }
